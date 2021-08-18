7167bdde2391bec5fb1f5af6dea1877beb9cd81d
Agustin Avila <tinto.avila@gmail.com>
2021-08-18 00:16:14 -0300

    Agregado manejo de mayusculas

--- Clase01/geringoso.py
+++ Clase01/geringoso.py
@@ -1,10 +1,10 @@
-vocales = 'aeiou'
+vocales = 'aAeEiIoOuU'
 print("Este programa devuelve la frase que se le provea, pero en geringoso.")
 frase = str(input("Por favor, ingrese una frase: "))
 frase_gerin = ''
 for caracter in frase:
     frase_gerin = frase_gerin + caracter
     if caracter in vocales:
-        frase_gerin = frase_gerin + 'p' + caracter
+        frase_gerin = frase_gerin + 'p' + caracter.lower()
 
 print(frase_gerin)
