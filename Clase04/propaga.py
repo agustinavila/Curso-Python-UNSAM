89c2150f09f5076573b449fa2bdcc487628a0488
agustinavila <tinto.avila@gmail.com>
2021-09-06 18:13:32 -0300

    A los golpes pero funcionando. Solucion poco elegante

--- Clase04/propaga.py
+++ Clase04/propaga.py
@@ -1,15 +1,29 @@
+# Está medio desprolijo pero funciona
+
 #%%
 def propagar(fosforos):
     fosforos_nuevo = []
     for i, fosforo in enumerate(fosforos):
-        if fosforo == 0 and i != 0:
-            if fosforos[i-1] == 1:
-                fosforos_nuevo.append(1)
-                continue
-        elif fosforo == 0 and i < len(fosforos)-1:   
-            if fosforos[i+1] == 1:
-                fosforos_nuevo.append(1)
-        else:
-            fosforos_nuevo.append(fosforos[i])
-    return fosforos_nuevo
+        if fosforo == 1:
+            quemado = False
+            pos = i + 1
+            while not quemado and pos < len(fosforos):
+                if fosforos[pos] == 0:
+                    fosforos[pos] = 1
+                elif fosforos[pos] == -1:
+                    quemado = True
+                pos += 1
+            pos = i - 1
+            quemado = False
+            while not quemado and pos >=0:
+                if fosforos[pos] == 0:
+                    fosforos[pos] = 1
+                elif fosforos[pos] == -1:
+                    quemado = True
+                pos -= 1
+    return fosforos
+# %%
+propagar([ 0, 0, 0,-1, 1, 0, 0, 0,-1, 0, 1, 0, 0])
+# %%
+propagar([ 0, 0, 0, 1, 0, 0])
 # %%
